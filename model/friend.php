775a86584610e3adaf4ba6a76f5e8b56ba0e5299
Yahya Khodr <yhy97kh18@gmail.com>
2022-02-13 23:18:27 +0200

    Implemented add friend api

---
+++ model/friend.php
@@ -0,0 +1,45 @@
+<?php
+
+class Friend
+{
+
+    private $con;
+
+    public $user_one;
+    public $user_two;
+    public $id;
+
+    public function __construct($db)
+    {
+        $this->con = $db;
+    }
+
+    public function addFriend()
+    {
+        if ($this->isAlreadyFriends()) {
+            return false;
+        } else {
+            $add_friend = $this->con->prepare("INSERT INTO friend_requests(sender_id,reciever_id) VALUES (?,?)");
+            $add_friend->bind_param("ii", $this->user_one, $this->user_two);
+            $add_friend->execute();
+            return $add_friend;
+        }
+    }
+
+
+    public function isAlreadyFriends()
+    {
+        $check_friends = $this->con->prepare("SELECT id FROM friend_requests WHERE sender_id = ? AND reciever_id = ?");
+        $check_friends->bind_param("ii", $this->user_one, $this->user_two);
+        $check_friends->execute();
+        $check_friends->store_result();
+        $check_friends->bind_result($this->id);
+        $check_friends->fetch();
+        $num_rows = $check_friends->num_rows;
+        if ($num_rows > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
