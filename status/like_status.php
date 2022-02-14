2cf12200cffe95d9074abdfc564008909143b8e2
Yahya Khodr <yhy97kh18@gmail.com>
2022-02-14 23:33:19 +0200

    Implemented the like status api

---
+++ status/like_status.php
@@ -0,0 +1,25 @@
+<?php
+
+include("../model/database.php");
+include("../model/status.php");
+
+
+$database = new Database();
+$db = $database->getConnection();
+$status = new Status($db);
+$post_id = $_POST["post_id"];
+
+if (isset($post_id)) {
+    if ($status->likeStatus($post_id)) {
+        $increment_arr = array(
+            "status" => true,
+            "message" => "Added a like from .'$post_id'.",
+        );
+    } else {
+        $increment_arr = array(
+            "status" => false,
+            "message" => "Failed to add a like",
+        );
+    }
+    print_r(json_encode($increment_arr));
+}
